30a314122d8dd7fd1a62b3c627cd190e7ab98d4c
Juan Carlos <jchernandez827@gmail.com>
2022-01-24 21:30:27 -0500

    Remove inspect

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(classroom, age, name = 'unknown', parent_permission: false)
+  def initialize(classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(specialization, age, name = 'unknown', parent_permission: false)
+  def initialize(specialization, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
