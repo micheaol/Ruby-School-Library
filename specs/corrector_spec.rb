fcd9827adbe658294bd849b5094a7efa58812312
micheaol <micheaol80@gmail.com>
2022-02-02 22:17:49 +0100

    Add test for corrector class

---
+++ specs/corrector_spec.rb
@@ -0,0 +1,15 @@
+require '../classes/corrector'
+
+describe Corrector do
+    describe '#validate_name' do
+        it 'returns a string with the first letter capitalized' do
+          name_instance = Corrector.new.correct_name('julio')
+          expect(name_instance).to eq('Julio')
+        end
+    
+        it 'returns a trim string if the string.length > 10' do
+          name_instance = Corrector.new.correct_name('juliocarlos')
+          expect(name_instance).to eq('Juliocarlo')
+        end
+      end
+end
