7748df224fb37aa6b7237ec4d45f519015884bdb
Juan Carlos <jchernandez827@gmail.com>
2022-02-02 16:13:51 -0500

    Linters fix

--- specs/classroom_spec.rb
+++ specs/classroom_spec.rb
@@ -2,23 +2,21 @@ require '../classes/classroom'
 require '../classes/student'
 
 describe Classroom do
-  context "Test Classroom class instances and methods" do
-    
-    it "When we create a new instace it should belong to Classroom class" do
+  context 'Test Classroom class instances and methods' do
+    it 'When we create a new instace it should belong to Classroom class' do
       fake_classroom_instance = Classroom.new('B4')
       expect(fake_classroom_instance).to be_an_instance_of(Classroom)
     end
 
-    describe "#add_student" do
-      it "Pushes a new instance of student class into an array, sets student instance classroom to be equal to self" do
+    describe '#add_student' do
+      it 'Pushes a new instance of student class into an array, sets student instance classroom to be equal to self' do
         fake_arr = []
         fake_student_instance = Student.new(25, 'John')
         fake_classroom_instance = Classroom.new('B4')
         fake_arr << fake_student_instance
         fake_classroom_instance.add_student(fake_student_instance)
-        expect(fake_arr.length).to be > 0
+        expect(fake_arr.length).to be.positive?
       end
     end
-  
   end
 end
--- specs/student_spec.rb
+++ specs/student_spec.rb
@@ -1,17 +1,17 @@
 require '../classes/student'
 
 describe Student do
-  context "test student class methods an instances" do
-     it "check for new instances to be student class instance" do
+  context 'test student class methods an instances' do
+    it 'check for new instances to be student class instance' do
       fake_student_instance = Student.new(28, 'mark')
       expect(fake_student_instance).to be_an_instance_of(Student)
-     end
+    end
 
-     describe "#play_hooky" do
+    describe '#play_hooky' do
       it "should return ¯\(ツ)/¯" do
         fake_student_instance = Student.new(32, 'joe')
         expect(fake_student_instance.play_hooky).to eq("¯\(ツ)/¯")
       end
-     end
+    end
   end
 end
