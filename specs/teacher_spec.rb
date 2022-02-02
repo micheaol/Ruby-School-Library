dfc0ddbeb60a5e17b2fae3957db4aed77a17cbb5
micheaol <micheaol80@gmail.com>
2022-02-02 21:49:42 +0100

    Add unit test for teacher class

---
+++ specs/teacher_spec.rb
@@ -0,0 +1,18 @@
+require '../classes/teacher'
+
+describe Teacher do
+    context "Create a teacher instance with valid input" do 
+       
+        it "When we create a new instace it should belong to Teacher class" do 
+           teacher_instance = Teacher.new("Vet", 35, "Paulina")
+           expect(teacher_instance).to be_an_instance_of(Teacher)
+        end
+     end
+
+     describe "#can_use_services?" do
+        it "Should return true" do
+        teacher_instance = Teacher.new("Vet", 35, "Paulina")
+        expect(teacher_instance.can_use_services?).to be(true)
+        end
+     end
+end
