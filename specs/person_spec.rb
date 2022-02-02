fc6ad346755fc69e11a593c5e0338dcd439300bf
micheaol <micheaol80@gmail.com>
2022-02-02 21:09:31 +0100

    Add test for add rental in person class

--- specs/person_spec.rb
+++ specs/person_spec.rb
@@ -1,5 +1,6 @@
 require '../classes/person'
 require '../classes/corrector'
+require '../classes/rental'
 
 
 describe Person do 
@@ -28,6 +29,18 @@ describe Person do
             name_instance = Corrector.new.correct_name('julio')
             expect(name_instance).to eq("Julio")
         end
+
+        it "returns a trim string if the string.length > 10" do
+            name_instance = Corrector.new.correct_name('juliocarlos')
+            expect(name_instance).to eq("Juliocarlo")
+        end
     end
 
+    describe "#add_rental" do
+        it "When we run add_rental method it should return an instance of rental" do
+            person_instance = Person.new('Lord of the rings', 'Joe')
+            add_rental = person_instance.add_rental('05/05/1900', 'John')
+            expect(add_rental).to be_an_instance_of(Rental)
+         end
+    end
   end
